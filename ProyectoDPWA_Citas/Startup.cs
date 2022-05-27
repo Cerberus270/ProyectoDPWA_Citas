d271b5630b9fb20bfb8e05ebde893ee87960d5a8
Cerberus270 <geoavila718@gmail.com>
2022-05-26 20:16:51 -0600

    Agregando Razor Runtime Compilation y terminando 4040

--- ProyectoDPWA_Citas/Startup.cs
+++ ProyectoDPWA_Citas/Startup.cs
@@ -25,7 +25,7 @@ namespace ProyectoDPWA_Citas
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(option => {
                     option.LoginPath = "/Acceso/Login";
